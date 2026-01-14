8c556bacdaa3dfa1099c8c9686bcfc7925534f42
Collin Wischmcj <cjwischmeyer@gmail.com>
2026-01-14 18:12:24 -0500

    removing unneeded functions, comments, etc from the utils module

--- pydar_utils/utils/io.py
+++ pydar_utils/utils/io.py
@@ -8,12 +8,16 @@ from set_config import log, config
 from numpy import array as arr
 from itertools import product
 from prettytable import PrettyTable
+import laspy
 
 # from open3d.io import read_point_cloud
 
 use_super_user = config['io']['super_user']
 data_root = config['io']['data_root']
 
+# def read_pcd(file, root_dir = data_root):
+#     pcd = read_point_cloud
+#     return pcd
 
 def save_line_set(line_set, base_file = 'skel_stem20_topology',root_dir=data_root):
     base_file.replace('.pkl','')
@@ -29,20 +33,12 @@ def load_line_set(base_file = 'skel_stem20_topology',root_dir=data_root):
     line_set.lines = o3d.utility.Vector2iVector(lines)
     return line_set
 
-def be_root():
-    # if os.geteuid() == 0:
-    return 1
-    # else:
-        # subprocess.call(['sudo', 'python3'] + sys.argv)  # modified
-
 def update(file, to_write, root_dir = data_root):
-    # be_root()
     curr = load(file,root_dir)
     curr.extend(to_write)
     save(file,curr,root_dir)
 
 def save(file, to_write, root_dir = data_root):
-    # be_root()
     if '.pkl' not in file: file=f'{file}.pkl'
     fqp = f'{root_dir}{file}'
 
@@ -50,7 +46,6 @@ def save(file, to_write, root_dir = data_root):
         pickle.dump(to_write,f)
 
 def load(file, root_dir = data_root):
-    # be_root()
     if '.pkl' not in file: file=f'{file}.pkl'
     if root_dir[-1] != '/': root_dir += '/'
     fqp = f'{root_dir}{file}'
@@ -74,12 +69,10 @@ def get_attrs_las(las_file, header=None):
     green = las_file.green
     points = np.vstack((x, y, z)).T
     colors = np.vstack((red, green, blue)).T
-    intensity = las_file.intensity
     data = np.hstack([points,colors, np.arange(len(x))[:,np.newaxis]])
     return data
 
 def convert_las(file_name, file_dir='', ext='pcd'):
-    import laspy
     orig_ext = file_name.split('.')[-1]
     # file_name = 'EpiphytusTV4.pts'
     # # file_dir = 'data/epip/inputs'
@@ -88,8 +81,6 @@ def convert_las(file_name, file_dir='', ext='pcd'):
     las = laspy.read(file)
     data = get_attrs_las(las)
 
-    if ext == 'las':
-        return data
     if ext == 'pcd':
         pts = data[:, :3]
         colors = data[:, 3:6]/65280
@@ -98,16 +89,8 @@ def convert_las(file_name, file_dir='', ext='pcd'):
         pcd.colors = o3d.utility.Vector3dVector(colors)
         o3d.io.write_point_cloud(f'/{file_name.replace(orig_ext,'.pcd')}', pcd)
         return pcd
-    elif ext == 'npy':
-        data = np.hstack([pts, colors])
-        np.save(f'/{file_name.replace(orig_ext,'.npy')}', data)
+    else: 
         return data
-    elif ext == 'npz':
-        data = np.hstack([pts, colors])
-        np.savez_compressed(f'/{file_name.replace(orig_ext,'.npz')}', data)
-        return data
-    else:
-        raise ValueError(f'Invalid extension {ext}')
 
 def np_to_o3d(npz_file):
     data = np.load(npz_file)
@@ -130,7 +113,6 @@ def to_o3d(coords=None, colors=None, las=None):
     pcd.points = o3d.utility.Vector3dVector(coords)
     if colors is not None:
         pcd.colors = o3d.utility.Vector3dVector(np.asarray(colors))
-    # labels = labels.astype(np.int32)
     return pcd
 
 def create_table(
@@ -176,52 +158,3 @@ def create_table(
         myTable.add_row([row_id] + row)
     print(myTable)
     return myTable
-
-def read_in_pcd_in_parts(file_prefix = 'data/input/SKIO/part_skio_raffai',
-                    return_pcd_list =False,
-                    return_single_pcd = False,
-                    return_list = None,
-                    return_lambda = None):
-    """loops over a list of files, reading in each as a point cloud to return as a list.
-        Used to read in the skio scan which is broken into grid blocks for processing.
-        . reads in the gridded sections 
-
-    Args:
-        file_prefix (str, optional): _description_. Defaults to 'data/input/SKIO/part_skio_raffai'.
-        return_pcd_list (bool, optional): _description_. Defaults to False.
-        return_single_pcd (bool, optional): _description_. Defaults to False.
-        return_list (_type_, optional): _description_. Defaults to None.
-        return_lambda (_type_, optional): _description_. Defaults to None.
-
-    Returns:
-        _type_: _description_
-    """
-    if return_lambda: return_list = []
-    else:
-        if not return_single_pcd and not return_pcd_list:
-            return None, None, None 
-    extents = {}
-    pcds = []
-    pts = []
-    colors = []
-    contains_region= []
-    base = 20000000
-
-    for i in range(40):
-        num = base*(i+1) 
-        file = f'{file_prefix}_{num}.pcd'
-        pcd = read_point_cloud(file)
-        if return_lambda:
-            return_list.append(return_lambda(pcd))
-        if return_pcd_list:
-            pcds.append(pcd)
-        elif return_single_pcd:
-            pts.extend(list(arr(pcd.points)))   
-            colors.extend(list(arr(pcd.colors)))
-    if return_single_pcd:
-        collective = o3d.geometry.PointCloud()
-        collective.points = o3d.utility.Vector3dVector(pts)
-        collective.colors = o3d.utility.Vector3dVector(colors)   
-        pcds.append(pcd)     
-    return return_list, contains_region, pcds
-    
